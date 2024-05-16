ae606880777c71ccbe313b51ed25eb160e9efb3e
Rodrigo Asafh <139464196+rasafhdev@users.noreply.github.com>
2024-05-15 23:28:29 -0300

    update

--- notificacao_telegram.py
+++ notificacao_telegram.py
@@ -1,19 +1,32 @@
-# main.py
-
+import telebot
 import time
+import dotenv
 import os
 from datetime import datetime
-from notificacao_telegram import Notificacao  # Importa a classe Notificacao do arquivo notificacao.py
 
+
+
+class Notificacao:
+    def __init__(self, TOKEN):
+        self.TOKEN = TOKEN
+        
+
+    def enviar_notificacao(self, mensagem):
+        self.bot = telebot.TeleBot(self.TOKEN)
+        self.bot.send_message(7021296280, mensagem)
+
+'''
 if __name__ == '__main__':
+
     while True:
         # Instancia
         notificacao = Notificacao(os.getenv("KEY"))
 
-        # Pega a hora do ponto
+        # pega a hora do ponto
         hora_capturada = datetime.now().strftime("%H:%M:%S")
         
-        # Envia a mensagem
+        # Envia a mensagem.
         mensagem = f'Ponto Registrado! {hora_capturada}'
         notificacao.enviar_notificacao(mensagem)
         time.sleep(60)
+'''
---
+++ robo.py
@@ -0,0 +1,26 @@
+import pyautogui
+
+
+teclas = pyautogui.KEYBOARD_KEYS
+
+# Imprimir todas as teclas disponíveis
+print(teclas)
+'''
+class RoboPonto:
+    def __init__(self):
+        pass
+
+    def tarefas_robo(self):
+        
+        # abrir menu iniciar
+        pyautogui.press('win')
+        # digitar brave
+        # abrir procurar pelo brave ou google chrome
+        # a
+
+if __name__ == '__main__':
+
+    robo = RoboPonto()
+
+    robo.tarefas_robo()
+'''
